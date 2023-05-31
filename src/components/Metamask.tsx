86166b6f1fb1c151a5c8ff22c3b871ee2429d4af
Stroke9 <clement.raoul65@gmail.com>
2023-05-31 21:09:38 +0200

    fix w- to metamask button

--- src/components/Metamask.tsx
+++ src/components/Metamask.tsx
@@ -84,7 +84,7 @@ const Metamask = () => {
 
   return (
     <div className="text-right">
-      <button onClick={handleMetamask} type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 w-6 h-6">
+      <button onClick={handleMetamask} type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">
         <Image
           className="mr-2"
           src="/metamask.svg"
